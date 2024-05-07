eab31477c3b6561963df855a0844fcb7c50cce57
Justine Nicolao <bororio96@gmail.com>
2024-05-07 06:51:19 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,16 @@
+function skillsMembers() {
+    var members = [
+        {
+            name: 'John Doe',
+            age: 25,
+            skills: ['HTML', 'CSS', 'JS']
+        },
+        {
+            name: 'Jane Doe',
+            age: 26,
+            skills: ['HTML', 'CSS', 'JS', 'React']
+        }
+    ];
+
+    return members;
+}
